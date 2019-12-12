ed19bd8b444d2ef79d8d51965d3aae8961f499aa
Om Bikram Rana <ome.bikram@gmail.com>
2019-12-12 11:53:31 +0100

    pikachuGrowl

--- puko/Assets/Scripts/FightScene/Pikachu/pikachuControlScript.cs
+++ puko/Assets/Scripts/FightScene/Pikachu/pikachuControlScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class pikachuControlScript : MonoBehaviour
 {
+    bool pikachuMove;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class pikachuControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(GameObject.FindGameObjectWithTag("CharizardGo") != null)
+        {
+            transform.LookAt(GameObject.FindGameObjectWithTag("CharizardGo").transform.position);
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+
+    }
+    public void Growl()
+    {
+        gameObject.GetComponent<Animator>().Play("Attack");
+        pikachuMove = true;
     }
 }
