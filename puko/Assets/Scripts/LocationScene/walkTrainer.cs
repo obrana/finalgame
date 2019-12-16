5f9f97f84e263a12318d8e64de6172da9d236d63
Om Bikram Rana <ome.bikram@gmail.com>
2019-12-16 23:31:02 +0100

    attacks

---
+++ puko/Assets/Scripts/LocationScene/walkTrainer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkTrainer : MonoBehaviour
+{
+    public Animation walk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            walk.Play();
+            StartCoroutine(Wait());
+        }
+
+        else if (!transform.hasChanged)
+        {
+            walk.Stop();
+        }
+        
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+        transform.hasChanged = false;
+    }
+} 
